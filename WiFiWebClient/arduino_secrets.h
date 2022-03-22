c9851f9f79f6a3365092bd4a027902e9466b70b9
Kent Pedersen <webzone@kpdesign.dk>
2022-03-22 21:07:47 +0100

    Add project files.

---
+++ WiFiWebClient/arduino_secrets.h
@@ -0,0 +1,6 @@
+//This file should be excluded from Github
+#define SECRET_SSID "BavnelyIOT"
+#define SECRET_PASS "Michelle@1903!"
+
+// #define SECRET_SSID "Kilgharrah"
+// #define SECRET_PASS "Code1234@"
