4139d6ab48e17be759859f371bb1f1da5684f457
Kent Pedersen <webzone@kpdesign.dk>
2022-03-22 21:20:06 +0100

    deleted arduino secrets

--- WiFiWebClient/arduino_secrets.h
+++
@@ -1,6 +0,0 @@
-//This file should be excluded from Githubber
-#define SECRET_SSID "BavnelyIOT"
-#define SECRET_PASS "Michelle@1903!"
-
-// #define SECRET_SSID "Kilgharrah"
-// #define SECRET_PASS "Code1234@"
