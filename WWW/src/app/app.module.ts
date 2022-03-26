a9248a894c16513bc2963dea78e83ea7238812b8
Webzonedk <webzone@kpdesign.dk>
2022-03-26 15:07:36 +0100

    Adhusted the app.module with ReactiveFormsModule and HttpClientModule

--- WWW/src/app/app.module.ts
+++ WWW/src/app/app.module.ts
@@ -1,4 +1,6 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,14 +9,16 @@ import { ListDataComponent } from './listData/listData.component';
 import { ShowGraphComponent } from './showGraph/showGraph.component';
 
 @NgModule({
-  declarations: [		
+  declarations: [
     AppComponent,
       ListDataComponent,
       ShowGraphComponent
    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
